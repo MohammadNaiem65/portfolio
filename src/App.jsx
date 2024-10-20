6b621cee5dba391bc1a61d5ddf7f12652f051776
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-10-20 07:17:11 +0600

    update: completed the Navbar design

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,14 @@
-import { useState } from 'react';
-import './App.css';
+import Content from './components/content/Content';
+import Navbar from './components/Navbar/Navbar';
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
-        <div className='min-h-dvh bg-[radial-gradient(ellipse_at_center,_#1b2735_0%,_#090a0f_100%)] text-slate-400'>
-            <div></div>
-            <h1>Vite + React</h1>
-            <div className='card'>
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className='read-the-docs'>
-                Click on the Vite and React logos to learn more
-            </p>
-        </div>
+        <section className='min-h-dvh bg-[radial-gradient(ellipse_at_center,_#1b2735_0%,_#090a0f_100%)] text-slate-400 grid grid-cols-7'>
+            <Navbar />
+            <Sidebar />
+            <Content />
+        </section>
     );
 }
 
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,36 @@
+import { useRef } from 'react';
+import './Sidebar.css';
+
+const LINKS = ['home', 'profile', 'projects', 'contact', 'hire me'];
+
 export default function Sidebar() {
+    const linksContainerRef = useRef(null);
+
     return (
-        <section className='col-span-2 h-[calc(100dvh-5rem)] flex items-center justify-center relative'>
-            Sidebar
+        <section
+            id='links-container'
+            ref={linksContainerRef}
+            className='col-span-2 h-full flex items-center justify-center relative'
+        >
+            <div
+                id='links'
+                className='w-fit text-5xl uppercase font-bold text-white flex flex-col gap-y-3 relative z-10'
+            >
+                {LINKS.map((link, index) => (
+                    <a
+                        key={index}
+                        className='link w-fit'
+                        href={`#${link === 'hire me' ? 'hire-me' : link}`}
+                        onMouseOver={() =>
+                            (linksContainerRef.current.dataset.index = index)
+                        }
+                    >
+                        {link}
+                    </a>
+                ))}
+            </div>
+
+            <div id='patterns' className='absolute inset-0 -z-0' />
         </section>
     );
 }
