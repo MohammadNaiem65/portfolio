6649f0f384a42577cce70a0165a0eabf7a1158e2
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-10-23 17:05:09 +0600

    fix: made the SlideDetailsModal responsive

--- src/components/content/Projects/SlideDetailsModal.jsx
+++ src/components/content/Projects/SlideDetailsModal.jsx
@@ -1,4 +1,5 @@
 import { motion } from 'framer-motion';
+import crossImg from '../../../assets/icons/cross-dark.png';
 
 export default function SlideDetailsModal({
     layoutId,
@@ -14,33 +15,42 @@ export default function SlideDetailsModal({
             onClick={() => setSelectedSlide(null)}
         >
             <section
-                className='w-[46rem] h-[25rem]'
+                className='w-[46rem] h-[25rem] relative'
                 onClick={(e) => e.stopPropagation()}
             >
                 <motion.img src={src} alt={alt} layoutId={layoutId + 1} />
 
-                <div className='h-14 mt-4 font-agdasima text-lg grid grid-cols-3'>
+                <div className='absolute top-4 right-4'>
+                    <img
+                        src={crossImg}
+                        alt='cross'
+                        className='size-4 md:size-6 cursor-pointer'
+                        onClick={() => setSelectedSlide(null)}
+                    />
+                </div>
+
+                <div className='h-14 mt-4 px-4 font-agdasima text-lg grid grid-cols-3'>
                     <h3 className='col-span-1 self-center text-3xl'>{title}</h3>
 
-                    <div className='col-span-2 self-center ml-auto'>
+                    <div className='col-span-2 self-end md:self-center ml-auto'>
                         <a
                             href={link}
                             target='_blank'
-                            className='w-fit mb-20 px-6 py-1 border-2 border-sky-400 font-semibold font-agdasima text-2xl text-sky-400 rounded duration-300 hover:bg-sky-400 hover:text-white'
+                            className='w-fit mb-20 md:px-6 md:py-1 md:border-2 border-sky-400 font-normal md:font-semibold font-agdasima text-lg md:text-2xl text-sky-400 rounded duration-300 hover:bg-sky-400 hover:text-white'
                         >
                             Live Link
                         </a>
                         <a
                             href={clientRepo}
                             target='_blank'
-                            className='w-fit mx-3 mb-20 px-6 py-1 border-2 border-sky-400 font-semibold font-agdasima text-2xl text-sky-400 rounded duration-300 hover:bg-sky-400 hover:text-white'
+                            className='w-fit mx-3 mb-20 md:px-6 md:py-1 md:border-2 border-sky-400 font-normal md:font-semibold font-agdasima text-lg md:text-2xl text-sky-400 rounded duration-300 hover:bg-sky-400 hover:text-white'
                         >
                             Client Repo
                         </a>
                         <a
                             href={serverRepo}
                             target='_blank'
-                            className='w-fit mb-20 px-6 py-1 border-2 border-sky-400 font-semibold font-agdasima text-2xl text-sky-400 rounded duration-300 hover:bg-sky-400 hover:text-white'
+                            className='w-fit mb-20 md:px-6 md:py-1 md:border-2 border-sky-400 font-normal md:font-semibold font-agdasima text-lg md:text-2xl text-sky-400 rounded duration-300 hover:bg-sky-400 hover:text-white'
                         >
                             Server Repo
                         </a>
